c7b7cdf6bc81d515d78d76fd8714d74e856ea06a
Petriona <108653563+Petriona@users.noreply.github.com>
2022-08-21 17:50:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -162,9 +162,10 @@ async def inline_handlers(bot, inline):
                 description="Documentation of Hagadmansa Bot ⚡️",
                 caption="Documentation of Hagadmansa Bot ⚡️",
                 thumb_url="https://telegra.ph/file/8c4c3ccf01f31538f6df9.jpg",
-                reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton("PirateBay", switch_inline_query_current_chat="!pb ",
-                     InlineKeyboardButton("YTS", switch_inline_query_current_chat="!yts "))]])
+                reply_markup=InlineKeyboardMarkup(
+                  [[InlineKeyboardButton("PirateBay", switch_inline_query_current_chat="!pb "),
+                    InlineKeyboardButton("YTS", switch_inline_query_current_chat="!yts ")]]
+                )
             )
         )
     
