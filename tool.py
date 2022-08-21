7e07086cff35619b18d83e66cc3e14c6cccf4f12
Petriona <108653563+Petriona@users.noreply.github.com>
2022-08-21 17:51:12 +0530

    Update tool.py

--- tool.py
+++ tool.py
@@ -6,7 +6,6 @@ from youtubepythonsearch import VideosSearch
 API_1337x = "https://api.abir-hasan.tk/1337x?query={}&limit={}"
 API_YTS = "https://api.abir-hasan.tk/yts?query={}&limit={}"
 API_PIRATEBAY = "https://api.abir-hasan.tk/piratebay?query={}&limit={}"
-API_ANIME = "https://api.abir-hasan.tk/anime?query={}&limit={}"
 
 async def YouTubeSearch(query):
     sear = VideosSearch(query)
@@ -28,10 +27,4 @@ async def SearchYTS(query: str):
 async def SearchPirateBay(query: str):
     async with aiohttp.ClientSession() as session:
         async with session.get(requote_uri(API_PIRATEBAY.format(query, 50))) as res:
-            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
-
-
-async def SearchAnime(query: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(requote_uri(API_ANIME.format(query, 50))) as res:
             return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
